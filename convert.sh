c86fb28f242d758c389a5be097b7702371a06c00
Sebastian Schmied <sebastian.schmied@1und1.de>
2017-05-18 16:21:37 +0200

    more waypoints

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl  -v "https://www.google.com/maps/d/kml?forcekml=1&mid=1BQW2L9hn454ky5jYWdMswSCpwMk&lid=FXac77ltWUg" -o nordschwarzwald.kml
+curl  -v -H 'Cache-Control: no-cache' "https://www.google.com/maps/d/kml?forcekml=1&mid=1BQW2L9hn454ky5jYWdMswSCpwMk&lid=FXac77ltWUg" -o nordschwarzwald.kml
 gpsbabel -w -i kml -f nordschwarzwald.kml -o cup -F nordschwarzwald.cup
 gpsbabel -w -i kml -f nordschwarzwald.kml -o ozi -F nordschwarzwald_ozi.wpt
 gpsbabel -w -i kml -f nordschwarzwald.kml -o compegps -F nordschwarzwald_compe.wpt
