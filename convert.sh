8405e2ae6b3392fae5738c4d1dc35c9741ec50df
Sebastian Schmied <sebastian.schmied@1und1.de>
2017-05-19 17:16:09 +0200

    Remove optional timestamp from .gpx
    Resulted in an unnecessary change for each conversion

--- convert.sh
+++ convert.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-curl  -v -H 'Cache-Control: no-cache' "https://www.google.com/maps/d/kml?forcekml=1&mid=1BQW2L9hn454ky5jYWdMswSCpwMk&lid=FXac77ltWUg" -o nordschwarzwald.kml
+curl -H 'Cache-Control: no-cache' "https://www.google.com/maps/d/kml?forcekml=1&mid=1BQW2L9hn454ky5jYWdMswSCpwMk&lid=FXac77ltWUg" -o nordschwarzwald.kml
 gpsbabel -w -i kml -f nordschwarzwald.kml -o cup -F nordschwarzwald.cup
 gpsbabel -w -i kml -f nordschwarzwald.kml -o ozi -F nordschwarzwald_ozi.wpt
 gpsbabel -w -i kml -f nordschwarzwald.kml -o compegps -F nordschwarzwald_compe.wpt
 gpsbabel -w -i kml -f nordschwarzwald.kml -o gpx -F nordschwarzwald.gpx
-
+# Remove optional <time> element from .gpx to avoid git clutter
+sed -i '/.*<time>.*<\/time>.*/d' nordschwarzwald.gpx
 
 
 
