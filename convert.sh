3205ec4a55a8521ca13e8d1ab6a09851ec81c6cd
Sebastian Schmied <sebastian.schmied@1und1.de>
2017-12-15 12:53:39 +0100

    hardcode a bit less

--- convert.sh
+++ convert.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-curl -H 'Cache-Control: no-cache' "https://www.google.com/maps/d/kml?forcekml=1&mid=1BQW2L9hn454ky5jYWdMswSCpwMk&lid=FXac77ltWUg" -o nordschwarzwald.kml
-gpsbabel -w -i kml -f nordschwarzwald.kml -o cup -F nordschwarzwald.cup
-gpsbabel -w -i kml -f nordschwarzwald.kml -o ozi -F nordschwarzwald_ozi.wpt
-gpsbabel -w -i kml -f nordschwarzwald.kml -o compegps -F nordschwarzwald_compe.wpt
-gpsbabel -w -i kml -f nordschwarzwald.kml -o gpx -F nordschwarzwald.gpx
+FILENAME="nordschwarzwald"
+MAPSID="1BQW2L9hn454ky5jYWdMswSCpwMk&lid=FXac77ltWUg"
+curl -H 'Cache-Control: no-cache' "https://www.google.com/maps/d/kml?forcekml=1&mid=${MAPSID}" -o ${FILENAME}.kml
+gpsbabel -w -i kml -f ${FILENAME}.kml -o cup -F ${FILENAME}.cup
+gpsbabel -w -i kml -f ${FILENAME}.kml -o ozi -F ${FILENAME}_ozi.wpt
+gpsbabel -w -i kml -f ${FILENAME}.kml -o compegps -F ${FILENAME}_compe.wpt
+gpsbabel -w -i kml -f ${FILENAME}.kml -o gpx -F ${FILENAME}.gpx
 # Remove optional <time> element from .gpx to avoid git clutter
-sed -i '/.*<time>.*<\/time>.*/d' nordschwarzwald.gpx
+sed -i '/.*<time>.*<\/time>.*/d' ${FILENAME}.gpx
 
 
 
